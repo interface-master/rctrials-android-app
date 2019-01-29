cb952f9e184e6c707676d4e1d32a83dcf94a1098
Michael Vinogradov <interface.master@gmail.com>
2019-01-29 17:54:23 -0500

    adding base for api service

---
+++ app/src/main/java/ca/interfacemaster/surveyor/ApiService.java
@@ -0,0 +1,30 @@
+package ca.interfacemaster.surveyor;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+public class ApiService {
+    private static final String BASE_URL = "http://10.0.2.2/api/";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    private static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static void getByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(url, params, responseHandler);
+    }
+
+    private static void postByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(url, params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
