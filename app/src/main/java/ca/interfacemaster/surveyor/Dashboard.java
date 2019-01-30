04b62ac1af7bd347de3c5b580ec0c9396f910050
Michael Vinogradov <interface.master@gmail.com>
2019-01-29 23:32:32 -0500

    adding activity for registering into a givn trial and saving the tid+uuid in shared prefs

--- app/src/main/java/ca/interfacemaster/surveyor/Dashboard.java
+++ app/src/main/java/ca/interfacemaster/surveyor/Dashboard.java
@@ -1,7 +1,15 @@
 package ca.interfacemaster.surveyor;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -9,5 +17,71 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        checkConfig();
+    }
+
+    /**
+     * Will check for config changes when coming back from another activity.
+     */
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        checkConfig();
+    }
+
+    /**
+     * Checks for presence of TID and UUID and configures the view accordingly.
+     */
+    private void checkConfig() {
+        String PREF_TID = getString(R.string.pref_tid);
+        String PREF_UUID = getString(R.string.pref_uuid);
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        if( pref.contains(PREF_TID) && pref.contains(PREF_UUID) ) {
+            configureSurveys();
+        } else {
+            configureSetUpButton();
+        }
+    }
+
+    /**
+     * Sets up the view to register with a trial.
+     */
+    private void configureSetUpButton() {
+        TextView header = findViewById(R.id.textDashboardHeader);
+        TextView subHeader = findViewById(R.id.textDashboardSubheader);
+        Button button = findViewById(R.id.buttonSetUpTID);
+        header.setText(getString(R.string.welcome_header));
+        subHeader.setText(getString(R.string.welcome_sub_header));
+        button.setText(getString(R.string.welcome_set_up));
+        button.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivityForResult(
+                        new Intent(Dashboard.this, InputTrial.class),
+                        1
+                    );
+                }
+            }
+        );
+    }
+
+    /**
+     * Sets up the view to check for available surveys.
+     */
+    private void configureSurveys() {
+        TextView header = findViewById(R.id.textDashboardHeader);
+        TextView subHeader = findViewById(R.id.textDashboardSubheader);
+        Button button = findViewById(R.id.buttonSetUpTID);
+        header.setText(getString(R.string.welcome_header));
+        subHeader.setText(getString(R.string.registered_sub_header));
+        button.setText(getString(R.string.registered_check));
+        button.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO: add a way to check for surveys
+                }
+            }
+        );
     }
 }
