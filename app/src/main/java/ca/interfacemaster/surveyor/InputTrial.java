86361cca7eb7a234fdd9d35e59d9bcabaccd7302
Michael Vinogradov <interface.master@gmail.com>
2019-01-29 19:43:56 -0500

    adding new Input Trial Activity

---
+++ app/src/main/java/ca/interfacemaster/surveyor/InputTrial.java
@@ -0,0 +1,13 @@
+package ca.interfacemaster.surveyor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputTrial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_trial);
+    }
+}
