177109248a46ea7fc8d39fd6547b48a1ba6ff5c1
Michael Vinogradov <interface.master@gmail.com>
2019-02-02 23:07:20 -0500

    scaffolding a click handler

--- app/src/main/java/ca/interfacemaster/surveyor/Dashboard.java
+++ app/src/main/java/ca/interfacemaster/surveyor/Dashboard.java
@@ -3,11 +3,15 @@ package ca.interfacemaster.surveyor;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -26,13 +30,31 @@ import cz.msebera.android.httpclient.Header;
 public class Dashboard extends AppCompatActivity {
 
     public static RecyclerView mRecyclerView;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation);
 
+        setupNav();
         checkConfig();
+    }
+
+    private void setupNav() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    menuItem.setChecked(true);
+                    mDrawerLayout.closeDrawers();
+                    // TODO: add code here to actually do stuff when items are clicked
+                    return true;
+                }
+            }
+        );
     }
 
     /**
