a98176b8ea1c358c1d1176c25c61383716c239d3
Michael Vinogradov <interface.master@gmail.com>
2019-02-05 20:39:46 -0500

    changing it to a button

--- app/src/main/java/ca/interfacemaster/surveyor/SurveyAdapter.java
+++ app/src/main/java/ca/interfacemaster/surveyor/SurveyAdapter.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class SurveyAdapter extends RecyclerView.Adapter<SurveyAdapter.MyViewHold
     private List<Survey> surveyList;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView title;
+        public Button title;
 
         public MyViewHolder(View itemView) {
             super(itemView);
