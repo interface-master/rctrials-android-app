d0fc3afaa8d9bf8f7b68e491645903c24e9cbcc8
Michael Vinogradov <interface.master@gmail.com>
2019-03-24 13:20:12 -0400

    cleanup

--- app/src/main/java/ca/interfacemaster/surveyor/Dashboard.java
+++ app/src/main/java/ca/interfacemaster/surveyor/Dashboard.java
@@ -1,8 +1,6 @@
 package ca.interfacemaster.surveyor;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +13,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -27,7 +26,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import ca.interfacemaster.surveyor.adapters.SurveyAdapter;
